53a17cf3179fb50f087fd5a32cb4762781b46f5f
Grazyna Borowka-Magda <gow@spyro-soft.com>
2026-06-30 13:05:10 +0200

    Modification of the environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, devices } from "@playwright/test";
 
 const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? "http://localhost:3000";
+const resultsRoot = "test-result";
+const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: "./tests",
   timeout: 10 * 1000,
   fullyParallel: true,
-  reporter: [["html", { open: "never" }]],
+  outputDir: `${resultsRoot}/test-results`,
+  preserveOutput: "always",
+  reporter: [["html", { open: "never", outputFolder: "playwright-report", doNotInlineAssets: true }]],
   webServer: process.env.PLAYWRIGHT_BASE_URL
     ? undefined
     : {
@@ -17,7 +21,7 @@ export default defineConfig({
       },
   use: {
     baseURL,
-    trace: "on-first-retry",
+    trace: isCI ? "on-first-retry" : "on",
   },
   projects: [
     {
